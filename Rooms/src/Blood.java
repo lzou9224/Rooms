0c4ae3e8d200cc9e0bf22fd8a308b581bbee964c
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-12-15 09:14:48 -0500

    aa

--- Rooms/src/Blood.java
+++ Rooms/src/Blood.java
@@ -3,9 +3,12 @@ public class Blood {
 	Person occupant;
 	int xLoc,yLoc;
 	private boolean explored = false;
+	private boolean[] tubes;
+	private Person[] occupants;
 	
 	//print method, subject to change 
 	//fixed print by defining print in person -alyssa
+	
 	public void print()
 	{
 		if(!explored && occupant == null)
@@ -22,25 +25,42 @@ public class Blood {
 			System.out.print("[b]");
 	}
 	
-	
-	
-	
-	public Blood(int x, int y)
+	//do we need an item for the letters? -lingli
+	public Blood(int x, int y, boolean[] tubes, Person[] occupants)
 	{
 		xLoc = x;
 		yLoc = y;
+        this.tubes = tubes;
+        this.setOccupants(occupants);
+        this.explored = false;
 	}
 	public void enterRoom(Person x)
 	{
-		System.out.println("You enter a plain old room");
+		System.out.println("You enter the bloodstream!");
 		occupant = x;
 		x.setxLoc(this.xLoc);
 		x.setyLoc(this.yLoc);
 	}
 	
+    public boolean[] getTubes()
+    {
+    	return tubes;
+    }
+
+	
 	public void leaveRoom(Person x)
 	{
 		occupant = null;
+	}
+
+
+	public Person[] getOccupants() {
+		return occupants;
+	}
+
+
+	public void setOccupants(Person[] occupants) {
+		this.occupants = occupants;
 	}
 	
 }
--- Rooms/src/Mouth.java
+++ Rooms/src/Mouth.java
@@ -18,4 +18,44 @@ public class Mouth extends Blood
 		occupant = null;
 	}
 	
+	public void print()
+	{
+		if (getOccupants().length != 0)
+	    {
+	      System.out.print(getOccupants()[0].print());
+	    }
+	    else if (this.explored)
+	    {
+	      System.out.print("[ H ]");
+	    }
+	    else
+	    {
+	      System.out.print("[   ]");
+	    }
+
+	}
+
+	    public String toString()
+	    {
+	    	boolean[] doors = this.getTubes();
+	    	String response = "This is the mouth. It has doors to the ";
+	    	if (doors[0])
+	    	{
+	    		response += "N";
+	    	}
+	    	
+	    	if(doors[1]) {
+	    		response += " and E";
+	    	}
+	    	
+	    	if (doors[2]) {
+	    		response += " and S";
+	    	}
+	    	
+	    	if (doors[3]) {
+	    		response += " and W";
+	    	}
+	    	
+	    	return response;
+	    }
 }
