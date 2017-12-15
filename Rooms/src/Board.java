bed56f11fde19fa586667ea33d9afca40bd7e1b9
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2017-12-15 09:03:31 -0500

    ff

---
+++ Rooms/src/Board.java
@@ -0,0 +1,32 @@
+
+import blood.Blood;
+
+public class Board {
+	private Blood[][] schoolMap;
+
+	public Board(Blood[][] schoolMap)
+	    {
+	        this.schoolMap = schoolMap;
+	    }
+
+	    public void printMap()
+	    {
+
+	        for(Blood[] row : schoolMap)
+	        {
+	            for (Blood room : row)
+	            {
+	                room.print();
+	            }
+	            System.out.println();
+	        }
+	    }
+	    public Blood[][] getSchoolMap() {
+	        return schoolMap;
+	    }
+
+	    public void setSchoolMap(Blood[][] schoolMap) {
+	        this.schoolMap = schoolMap;
+	    }
+
+}
