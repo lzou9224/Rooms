14d4a71efe5ed73b07f6597c35bdfaf197c5b2f2
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-01-02 09:24:18 -0500

    sa

--- Rooms/src/Bladder.java
+++ Rooms/src/Bladder.java
@@ -19,7 +19,7 @@ public class Bladder extends Room
 	public Bladder(int x, int y, boolean[] tubes, Person occupant) {
 		super(x, y, tubes, occupant);
 		// TODO Auto-generated constructor stub
-	}
+	} 
 	
 	@Override
 	public void enterRoom(Person x) {
